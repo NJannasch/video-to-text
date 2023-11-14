2c09ebd9eb2824e4112f29627e649ae5fcc1935d
N Jannasch <NJannasch@users.noreply.github.com>
2023-11-14 14:23:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,30 @@
 
 import pytube
 import whisper
+import argparse
 
-# Configuration
-video = "https://www.youtube.com/watch?v=xLMbTSax_PI&t=9s"
-video_file="video.mp4"
+def main():
+    parser = argparse.ArgumentParser(description='Download and transcribe audio from a YouTube video')
+    parser.add_argument('--link', required=True, help='YouTube video link')
+    args = parser.parse_args()
 
-# Converting and downloading as 'MP4' file
-data = pytube.YouTube(video)
-audio = data.streams.get_audio_only()
-audio.download(output_path="", filename=video_file)
+    # Configuration
+    video = args.link
+    video_file = "video.mp4"
 
-model = whisper.load_model("base")
-text = model.transcribe(video_file)
+    # Converting and downloading as 'MP4' file
+    data = pytube.YouTube(video)
+    audio = data.streams.get_audio_only()
+    audio.download(output_path="", filename=video_file)
 
-# Printing the transcribe
-print(text['text'])
+    model = whisper.load_model("base")
+    text = model.transcribe(video_file)
+
+    # Printing the transcription
+    print("_______________________")
+    print("____ Video Content ____")
+    print("_______________________")
+    print(text['text'])
+
+if __name__ == "__main__":
+    main()
